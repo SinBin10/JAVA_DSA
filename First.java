3b695e813ca34476da2f2e0e81db9d91783078cc
SinBin10 <bincode10@gmail.com>
2025-08-26 08:30:49 +0530

    data types in java

--- First.java
+++ First.java
@@ -1,5 +1,10 @@
 public class First{
     public static void main(String []args){
         System.out.println("Hello AI World");
+        byte name = 127;
+        long phone = 1112234223423444L;
+        char c = 'w';
+        boolean isAdult = true;
+        System.out.println(name);
     }
 }
