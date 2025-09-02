365fb2aca9ed647a95b0d629c0ea686ea3ebb788
SinBin10 <bincode10@gmail.com>
2025-09-02 07:07:29 +0530

    learning about list and arraylist

--- First.java
+++ First.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class First{
     public static void main(String []args){
         System.out.println("Hello AI World");
@@ -9,6 +12,21 @@ public class First{
         boolean isAdult = true;
         String person = "me";
         String friend = new String("friends");
+        String name2 = "Binay";
+        String name3 = name2.substring(0,2);
         System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name2.length());
+
+        //fixed size array
+        int[] arr = new int[4];
+        arr[0] = 23;
+        System.out.println(arr[0]);
+
+        //dynamic size array, like vector in java
+        List<Integer> arr2 = new ArrayList<>();
+        arr2.add(234);
+        System.out.println(arr2);
     }
 }
